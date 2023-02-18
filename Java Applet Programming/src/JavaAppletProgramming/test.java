090144dfaf8659c35df1bada6ed1ddf3f924c35c
Prince Singh <thisisprincesingh@gmail.com>
2023-02-18 20:59:35 +0530

    Add files via upload
    add all files

---
+++ Java Applet Programming/src/JavaAppletProgramming/test.java
@@ -0,0 +1,19 @@
+package JavaAppletProgramming;
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame();
+        JButton b = new JButton("Submit");
+
+        f.add(b);
+        f.setVisible(true);
+        f.setSize(500,500);
+        f.setLocation(100,50);
+
+
+
+
+    }
+}
